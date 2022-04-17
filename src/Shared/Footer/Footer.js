f31bf0c42f99036e816a00cf61b82e8ba64f0eca
SanyFaysal <tfoysalahmed@gmail.com>
2022-04-17 10:12:49 +0600

    project initialized

---
+++ src/Shared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+
+    return (
+        <div>
+            <small className='text-center text-primary my-4'>Copy right © /dynamic year/</small>
+        </div>
+    );
+};
+
+export default Footer;
